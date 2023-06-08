908be87ba46250d84e03df3b5a36488c8aaa8814
bhh9860 <bhh9860@naver.com>
2023-06-08 15:32:14 +0900

    루트 띄우기까지는 완료

--- controller/index.js
+++ controller/index.js
@@ -33,28 +33,87 @@ exports.route = (req, res) => {
         routeDayResultArray = [...set1];
 
         models.detail.findAll().then((detailTable) => {
-            console.log(detailTable[0].dataValues)
+            // console.log(detailTable[0].dataValues)
             res.render('userinfo.ejs', { routeCity: routeCityResultArray, routeDay: routeDayResultArray, detailTable: detailTable });
         })
-
     });
 }
 
 //route_id를 통해 detail테이블의 정보 불러오기
 exports.detail = (req, res) => {
-    console.log(req.body)
-    // console.log(req.city)
-    models.route.findAll({
-        attributes: ["route_id"],
-        where: {
-            route_city: req.body.city,
-            route_day: req.body.day
-        }
-    }).then((data) => {
-        console.log('route_id는', data[0].dataValues.route_id)
-        models.detail.findAll().then((dataa) => {
-            console.log('detail테이블의 정보는', dataa[data[0].dataValues.route_id].dataValues.detail_comment)
-            res.send({ detailTable: dataa[data[0].dataValues.route_id].dataValues.detail_comment })
+    //지역과 날짜가 all일 때
+    if (req.body.city === 'all' && req.body.day === 'all') {
+        models.detail.findAll().then((data) => {
+            detailArray = [];
+            for (let j = 0; j < data.length; j++) {
+                detailArray.push(data[j].dataValues.detail_comment)
+            }
+            res.send(detailArray)
         })
-    })
+        //지역만 all일 때
+    } else if (req.body.city === 'all' && req.body.day !== 'all') {
+        console.log('aaaaaaaaaaaaa', req.body.city)
+        models.route.findAll({
+            attributes: ["route_id"],
+            where: {
+                route_day: req.body.day
+            }
+        }).then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                console.log('route_id는', data[i].dataValues.route_id)
+            }
+            models.detail.findAll().then((dataa) => {
+                detailArray = [];
+                for (let j = 0; j < data.length; j++) {
+                    detailArray.push(dataa[data[j].dataValues.route_id - 1].dataValues.detail_comment)
+                }
+                console.log('detailArray:', detailArray)
+                res.send(detailArray)
+            })
+        })
+        //날짜만 all일 때
+    } else if (req.body.city !== 'all' && req.body.day === 'all') {
+        models.route.findAll({
+            attributes: ["route_id"],
+            where: {
+                route_city: req.body.city
+            }
+        }).then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                console.log('route_id는', data[i].dataValues.route_id)
+            }
+            models.detail.findAll().then((dataa) => {
+                detailArray = [];
+                for (let j = 0; j < data.length; j++) {
+                    detailArray.push(dataa[data[j].dataValues.route_id - 1].dataValues.detail_comment)
+                }
+                console.log('detailArray:', detailArray)
+                res.send(detailArray)
+            })
+        })
+
+        // 지역과 날짜가 all이 아닐 때
+    } else {
+        // console.log(req.city)
+        models.route.findAll({
+            attributes: ["route_id"],
+            where: {
+                route_city: req.body.city,
+                route_day: req.body.day
+            }
+            //route테이블의 route_id
+        }).then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                // console.log('route_id는', data[i].dataValues.route_id)
+            }
+            models.detail.findAll().then((dataa) => {
+                detailArray = [];
+                for (let j = 0; j < data.length; j++) {
+                    detailArray.push(dataa[data[j].dataValues.route_id - 1].dataValues.detail_comment)
+                }
+                // console.log('detailArray:', detailArray)
+                res.send(detailArray)
+            })
+        })
+    }
 }

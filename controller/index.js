085cbde9314f188a21bb9c163cff5c812ad27811
Moonyeryeong <Moon021112@icloud.com>
2023-06-12 09:26:41 +0900

    sub-page-4

--- controller/index.js
+++ controller/index.js
@@ -10,8 +10,17 @@ exports.Cget_register = (req, res) => {
   res.render("register");
 };
 
-///
-exports.Cget_intro = (req, res) => {
-  res.render("intro");
+///상세페이지
+exports.Cget_intro_seoul = (req, res) => {
+  res.render("intro_seoul");
+};
+exports.Cget_intro_busan = (req, res) => {
+  res.render("intro_busan");
+};
+exports.Cget_intro_gangneung = (req, res) => {
+  res.render("intro_gangneung");
+};
+exports.Cget_intro_jeju = (req, res) => {
+  res.render("intro_jeju");
 };
 ///

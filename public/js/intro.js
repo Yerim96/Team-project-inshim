b24136f53d61c7233de360dbc0e7ad107bac81c0
Moonyeryeong <Moon021112@icloud.com>
2023-06-12 14:54:38 +0900

    수정

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,19 +1,1 @@
-// window.addEventListener("load", function () {
-//   var seoulIntroduce = document.querySelector(".seoul-introduce");
-//   seoulIntroduce.classList.add("show");
-// });
 
-// window.addEventListener("load", function () {
-//   var busanIntroduce = document.querySelector(".busan-introduce");
-//   busanIntroduce.classList.add("show");
-// });
-
-// window.addEventListener("load", function () {
-//   var gangneungIntroduce = document.querySelector(".gangneung-introduce");
-//   gangneungIntroduce.classList.add("show");
-// });
-
-// window.addEventListener("load", function () {
-//   var jejuIntroduce = document.querySelector(".jeju-introduce");
-//   jejuIntroduce.classList.add("show");
-// });
